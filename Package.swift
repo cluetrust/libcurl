7ece4f4ad01d5cd2357bb159550549648b178431
Gaige B Paulsen <gaige@cluetrust.net>
2022-04-30 08:41:15 -0400

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+ 
+import PackageDescription
+ 
+let package = Package(
+    name: "libcurl",
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "libcurl",
+            targets: ["libcurl"]),
+    ],
+    dependencies: [],
+    targets: [
+        .binaryTarget(name: "libcurl", path: "https://www.cluetrust.com/spm/libcurl-xcframework-7.83.0.zip",
+                       checksum: "2b14d923ce8b690cca8db48a596b4a4a6023c46f4fae8f7280fbb9016cf29606")
+    ]
+)
